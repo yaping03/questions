13491ed359ce6fdabb8bed1744a697be7b1f9d07
yaping03 <820974538@qq.com>
2017-12-06 16:03:29 +0800

    ..

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    """
+    员工表
+    """
+    name = models.CharField(max_length=32)
+
+class ClassList(models.Model):
+    """
+    班级表
+    """
+    title = models.CharField(max_length=32)
+
+class Student(models.Model):
+    """
+    学生表
+    """
+    user = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=32)
+    cls = models.ForeignKey(to=ClassList)
+
+class Questionnaire(models.Model):
+    """
+    问卷表
+    """
+    title = models.CharField(max_length=64)
+    cls = models.ForeignKey(to=ClassList)
+    creator = models.ForeignKey(to=UserInfo)
+
+class Question(models.Model):
+    """
+    问题
+    """
+    caption = models.CharField(max_length=64)
+
+    question_types = (
+        (1,'打分'),
+        (2,'单选'),
+        (3,'评价'),
+    )
+    tp = models.IntegerField(choices=question_types)
+
+class Option(models.Model):
+    """
+    单选题的选项
+    """
+    name = models.CharField(verbose_name='选项名称',max_length=32)
+    score = models.IntegerField(verbose_name='选项对应的分值')
+    qs = models.ForeignKey(to=Question)
+
+class Answer(models.Model):
+    """
+    回答
+    """
+    stu = models.ForeignKey(to=Student)
+    question = models.ForeignKey(to=Question)
+
+    option = models.ForeignKey(to="Option",null=True,blank=True)
+    val = models.IntegerField(null=True,blank=True)
+    content = models.CharField(max_length=255,null=True,blank=True)
--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse,redirect
+from django.forms import ModelForm,Form
+from app01 import models
+from django.forms import fields
+from django.forms import widgets as wd
 # Create your views here.
+class Quest(ModelForm):
+    class Meta:
+        model = models.Question
+        fields = ['caption','tp']
+
+def quest(request,id):
+    obj = models.Questionnaire.objects.filter(id=id).first()
+    que = models.Question.objects.all()
+    if obj:
+        form = Quest()
+
+        if request.method=="POST":
+            pass
+        return render(request,"request.html",{"que":que})
+    else:
+        return HttpResponse("问卷不存在")
